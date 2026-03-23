b76c2e5d62bd2fe26fc1f09906ea93e1f53387e8
Alfredo Gutiérrez <alfredo@alfredo.me>
2026-03-23 11:41:34 -0500

    Fix App Review auth and subscription issues

--- Talkdraft/Services/SupabaseClient.swift
+++ Talkdraft/Services/SupabaseClient.swift
@@ -6,6 +6,9 @@ enum AppConfig {
     static let supabaseAnonKey =
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmdHd2dWR1enp5bXF4ZHZrd3dkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2NTA3MTAsImV4cCI6MjA4NzIyNjcxMH0.LyFLwFsWTmpa55lFpTi0Pbk-FAuJDvJ5W5vlHCjb1sA"
     static let redirectURL = URL(string: "talkdraft://auth/callback")!
+    static let privacyPolicyURL = URL(string: "https://gutierrezfredo.github.io/talkdraft-ios/legal/privacy.html")!
+    static let termsOfUseURL = URL(string: "https://gutierrezfredo.github.io/talkdraft-ios/legal/terms.html")!
+    static let manageSubscriptionsURL = URL(string: "https://apps.apple.com/account/subscriptions")!
 }
 
 let supabase = SupabaseClient(
